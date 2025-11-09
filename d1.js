ad96bf5a1ba3ea56d6b5a5a38c27628c991e7032
Esmira Aghamedova <esmo.agamedova2020@gmail.com>
2025-11-09 17:49:28 +0400

    D1+D2

---
+++ d1.js
@@ -0,0 +1,5 @@
+const name = "Esmira";
+let age = 16;
+let city = "Georgia, Tbilisi"
+
+console.log(`my name is ${name}, I'm ${age} years old and I'm from ${city}.`)
---
+++ d2.js
@@ -0,0 +1,20 @@
+function sum(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+console.log(sum(3, 4))
+console.log(subtract(10, 5))
+console.log(multiply(2, 6))
+console.log(divide(30, 5))
